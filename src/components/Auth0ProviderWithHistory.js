625aaa0c739d0ba8639d95d3e444e30f62b01994
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2023-05-24 17:53:03 +1200

    add: auth0Provider component and protect index.js

--- src/components/Auth0ProviderWithHistory.js
+++ src/components/Auth0ProviderWithHistory.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const navigate = useNavigate();
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
-  return <div>Auth0Provider</div>;
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: audience,
+      }}
+    >
+      {children}
+    </Auth0Provider>
+  );
 };
 
 export default Auth0ProviderWithHistory;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Auth0ProviderWithHistory from "./components/Auth0ProviderWithHistory";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,7 +8,9 @@ import reportWebVitals from "./reportWebVitals";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
   </React.StrictMode>
 );
 
