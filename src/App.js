1ce43b842875b1802ece3435a6aaf0aa5bf17cd4
kylewuzhere <kyvasquez1026@gmail.com>
2023-08-10 13:00:26 +1200

    add demo route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Homepage from "./pages/Homepage";
+import Demopage from "./pages/Demopage.js";
 import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/demo" element={<Demopage />} />
+      </Routes>
     </div>
   );
 }
